ca04a2959865e758fe6b08f6e95cea96dd13c934
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-03 11:32:52 -0300

    Add, condig project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  ignorePatterns: [ "*.test.js", "*.test.tsx" ],
+  rules: {
+    "react/jsx-filename-extension": [ 1, { "extensions": [".js", ".jsx"] }],
+    "react/react-in-jsx-scope": "off"
+  },
+};
