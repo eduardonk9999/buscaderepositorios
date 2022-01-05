d223bf3dbd6adeeb47977714962e2190f4f3e9a5
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-05 09:48:56 -0300

    Add, ajuste de class

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus } from 'react-icons/fa'
 
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input
-          type="text"
-          placeholder='Adcionar repositório'
-         />
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+    this.state = {
+      newRepo: '',
+    };
+  }
+
+  handleInputChance = e => {
+    this.setState({ newRepo: e.targer.value })
+  }
+  render() {
+    const { newRepo } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={() => {}}>
+          <input
+            type="text"
+            placeholder='Adcionar repositório'
+            value={newRepo}
+            onChange={this.handleInputChance}
+           />
+
+          <SubmitButton disabled>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
 }
 
-export default Main;
+
